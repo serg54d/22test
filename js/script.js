d9236affe0c5cccfe667be95eec76903252e55ab
serg54d <serg54d@bk.ru>
2023-11-26 04:18:45 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.menu__burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener("click", function (e) {
+	menu.classList.toggle('_active')
+});
